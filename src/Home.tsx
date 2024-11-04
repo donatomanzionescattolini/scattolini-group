76b72975615c8243771ea795c3dd991b82a7cb95
donato manzione <donato.manz.sc@gmail.com>
2024-11-04 11:57:48 -0500

    m

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,41 +1,40 @@
-import { AboutUs } from "./AboutUs";
+import {AboutUs} from "./AboutUs";
 
-import { MDBBtn, MDBContainer } from "mdb-react-ui-kit";
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import {MDBContainer} from "mdb-react-ui-kit";
+import React, {useEffect, useLayoutEffect, useState} from "react";
 import AliadosComponent from "./components/AliadosComponent.tsx";
 import AsociadosSmallComponent from "./components/AsociadosSmallComponent.tsx";
 import Quotes from "./components/Quotes";
 import Servicios from "./components/Servicios";
 import AreasComponent from "./components/areas/AreasComponent.tsx";
 import DesarrollosTodos from "./components/desarrollos/DesarrollosComponent.tsx";
-import Quote, { quotes } from "./models/Quote";
-import FloatingContactFormComponent from "./components/FloatingContactFormComponent.tsx";
+import Quote, {quotes} from "./models/Quote";
 
 export function Home() {
-  const [width, setWidth] = React.useState(window.innerWidth);
+    const [width, setWidth] = React.useState(window.innerWidth);
 
-  useEffect(() => {
-    const handleResizeWindow = () => setWidth(window.innerWidth);
-    // subscribe to window resize event "onComponentDidMount"
-    window.addEventListener("resize", handleResizeWindow);
-    return () => {
-      // unsubscribe "onComponentDestroy"
-      window.removeEventListener("resize", handleResizeWindow);
-    };
-  }, []);
+    useEffect(() => {
+        const handleResizeWindow = () => setWidth(window.innerWidth);
+        // subscribe to window resize event "onComponentDidMount"
+        window.addEventListener("resize", handleResizeWindow);
+        return () => {
+            // unsubscribe "onComponentDestroy"
+            window.removeEventListener("resize", handleResizeWindow);
+        };
+    }, []);
 
-  useLayoutEffect(() => {
-    window.resizeTo(0, 0);
-  });
-  // State variable to store the visibility of the floating element
-  const [show, setShow] = useState(true);
+    useLayoutEffect(() => {
+        window.resizeTo(0, 0);
+    });
+    // State variable to store the visibility of the floating element
+    const [show, setShow] = useState(true);
 
-  // Function to toggle the visibility of the floating element
+    // Function to toggle the visibility of the floating element
 
-  const [quotez] = useState<Quote[]>(quotes);
-  return (
-    <>
-      {/* {show && (
+    const [quotez] = useState<Quote[]>(quotes);
+    return (
+        <>
+            {/* {show && (
         <div
           className={"floating-element object-fit-cover"}
           style={{ background: "rgb(255,255,255,0.0)" }}
@@ -44,76 +43,76 @@ export function Home() {
             Close
           </MDBBtn>
           {/* <FloatingContactFormComponent /> */}
-        {/* </div> */}
-      {/* )} */}
-      <MDBContainer fluid jumbotron>
-        <div className="video-container">
-          <video autoPlay loop muted playsInline className={" m-0 p-0"}>
-            <source
-              src="https://pagina-mama.s3.amazonaws.com/assets2/video-homepage.mp4"
-              type="video/mp4"
-            />
-          </video>
-          <div className="overlay"></div>
+            {/* </div> */}
+            {/* )} */}
+            <MDBContainer fluid jumbotron>
+                <div className="video-container">
+                    <video autoPlay loop muted playsInline className={" m-0 p-0"}>
+                        <source
+                            src="https://pagina-mama.s3.amazonaws.com/assets2/video-homepage.mp4"
+                            type="video/mp4"
+                        />
+                    </video>
+                    <div className="overlay"></div>
 
-          <div className="content text-center ">
-            <h1 className="display-2" id="hero-heading">SCATTOLINI GROUP</h1>
-            <p className="lead" id="hero-subheading">
-              Agencia Inmobiliaria
-            </p>
-            <a
-              className="btn btn-large btn-outline-secondary mt-3"
-              href="mailto:elda.scattolini@scattolinigroup.com"
-            >
-              Contáctanos Hoy
-            </a>
-          </div>
-        </div>
-      </MDBContainer>
-      {width < 768 && <br></br>}
-      <section className="white-block pb-0 mb-0 pt-5">
-        <AboutUs />
-      </section>
-      {/* <div> */}
-      {/* <h3 className="text-center">Otras Áreas</h3> */}
-      {/* </div> */}
-      <div className="skew-cc"></div>
+                    <div className="content text-center ">
+                        <h1 className="display-2" id="hero-heading">SCATTOLINI GROUP</h1>
+                        <h3 className="text-center" id="hero-subheading">
+                            Agencia Inmobiliaria
+                        </h3>
+                        <a
+                            className="btn btn-large btn-outline-secondary mt-3"
+                            href="mailto:elda.scattolini@scattolinigroup.com"
+                        >
+                            Contáctanos Hoy
+                        </a>
+                    </div>
+                </div>
+            </MDBContainer>
+            {width < 768 && <br></br>}
+            <section className="white-block pb-0 mb-0 pt-5">
+                <AboutUs/>
+            </section>
+            {/* <div> */}
+            {/* <h3 className="text-center">Otras Áreas</h3> */}
+            {/* </div> */}
+            <div className="skew-cc"></div>
 
-      <section className="colour-block">
-        <AreasComponent />
-      </section>
+            <section className="colour-block">
+                <AreasComponent/>
+            </section>
 
-      <div className="skew-c"></div>
-      <section className="white-block">
-        <MDBContainer>
-          <AsociadosSmallComponent />
-        </MDBContainer>
-      </section>
-      <div className="skew-cc"></div>
-      {/* <DividerSecondComponent /> */}
-      <br />
-      <section className="colour-block">
-        <MDBContainer>
-          <Servicios />
-        </MDBContainer>
-        <br />
-      </section>
-      <div className="skew-c"></div>
-      <section className="white-block">
-        <MDBContainer>
-          <DesarrollosTodos />
-        </MDBContainer>
-      </section>
-      <div className="skew-cc"></div>
-      <section className="colour-block">
-        <MDBContainer>
-          <AliadosComponent />
-        </MDBContainer>
-      </section>
-      <div className="skew-c"></div>
-      <section className="white-block">
-        <Quotes quotes={quotez} />
-      </section>
-    </>
-  );
+            <div className="skew-c"></div>
+            <section className="white-block">
+                <MDBContainer>
+                    <AsociadosSmallComponent/>
+                </MDBContainer>
+            </section>
+            <div className="skew-cc"></div>
+            {/* <DividerSecondComponent /> */}
+            <br/>
+            <section className="colour-block">
+                <MDBContainer>
+                    <Servicios/>
+                </MDBContainer>
+                <br/>
+            </section>
+            <div className="skew-c"></div>
+            <section className="white-block">
+                <MDBContainer>
+                    <DesarrollosTodos/>
+                </MDBContainer>
+            </section>
+            <div className="skew-cc"></div>
+            <section className="colour-block">
+                <MDBContainer>
+                    <AliadosComponent/>
+                </MDBContainer>
+            </section>
+            <div className="skew-c"></div>
+            <section className="white-block">
+                <Quotes quotes={quotez}/>
+            </section>
+        </>
+    );
 }
