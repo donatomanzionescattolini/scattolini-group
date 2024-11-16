8c33bbe502e99d9c9397c78211de412712534516
Donato Manzione <manzione@usc.edu>
2024-11-16 02:22:11 -0500

    m

--- src/components/areas/AreaTemplate.tsx
+++ src/components/areas/AreaTemplate.tsx
@@ -1,11 +1,11 @@
-import { Image } from "react-bootstrap";
-import { MDBCol, MDBRow } from "mdb-react-ui-kit";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import {Image} from "react-bootstrap";
+import {MDBCol, MDBRow} from "mdb-react-ui-kit";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
 import Desarrollo from "../../models/desarrollos/Desarrollo.tsx";
-import { Area } from "../../models/areas/Area.tsx";
-import { getDesarrollosForArea } from "../../objects/desarrollos/Desarrollos.ts";
+import {Area} from "../../models/areas/Area.tsx";
+import {getDesarrollosForArea} from "../../objects/desarrollos/Desarrollos.ts";
 
 interface AreaProps {
     area: Area;
@@ -33,91 +33,85 @@ export default function AreaTemplate(props: AreaProps) {
     return (
         <>
             <header
-                    className="w-100 p-0 m-0 d-flex flex-row justify-content-center align-items-center align-content-center bg-image "
-                    style={{
-                        backgroundImage:
-                            `url('https://pagina-mama.s3.amazonaws.com/assets2/areas/${nombre}/banner.webp')`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                        overflow: "hidden",
-                        height: "10rem"
-                    }}
-                >
-                    <div className={"mask  w-100 h-100 justify-content-center align-content-center "}
-                        style={{ backgroundColor: "rgba(0,0,0,0.2)" }}>
-                       
-                    </div>
+                className="w-100 p-0 m-0 d-flex flex-row justify-content-center align-items-center align-content-center bg-image "
+                style={{
+                    backgroundImage:
+                        `url('https://pagina-mama.s3.amazonaws.com/assets2/areas/${nombre}/banner.webp')`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    overflow: "hidden",
+                    height: "10rem"
+                }}
+            >
+                <div className={"mask  w-100 h-100 justify-content-center align-content-center "}
+                     style={{backgroundColor: "rgba(0,0,0,0.2)"}}>
 
-                    <div className="row mask d-flex align-content-center"> <div className="col text-center text-white"> <h1>{titulo}</h1> <h2 className="subtitle">{area.slogan}</h2> </div></div>
+                </div>
+
+                <div className="row mask d-flex align-content-center">
+                    <div className="col text-center text-white"><h1>{titulo}</h1> <h2
+                        className="subtitle">{area.slogan}</h2></div>
+                </div>
 
             </header>
             <div className="skew-c"></div>
             <section className="white-block mb-auto">
                 <MDBRow className="ps-lg-5 ps-xl-5 ps-md-0 px-sm-1 px-xs-1 align-items-center overflow-visible">
 
-                    <MDBCol xs={12} sm={12} md={6} lg={8} xl={8} className=" d-flex flex-column justify-content-evenly align-items-center overflow-visible py-5">
-                  
+                    <MDBCol xs={12} sm={12} md={6} lg={8} xl={8}
+                            className=" d-flex flex-column justify-content-evenly align-items-center overflow-visible py-5">
+
                         {area.descripcion.map((paragraph: string) => {
-                            return (<p className="px-auto overflow-visible z-5">{paragraph}<br /></p>)
+                            return (<p className="px-auto p-sm-5 p-xs-5 overflow-visible z-5">{paragraph}<br/></p>)
                         })}
 
 
                     </MDBCol>
                     <MDBCol className={"ms-md-2 ms-sm-0 m-auto d-flex justify-content-center"}>
-                    <img
-                        
-                        src={firstImage}
-                        className="  img-fluid img-thumbnail flex-shrink-1"
-                    />
+                        <img
+
+                            src={firstImage}
+                            className="  img-fluid img-thumbnail flex-shrink-1"
+                         alt={area.name}/>
 
                     </MDBCol>
                 </MDBRow>
             </section>
             <div className="skew-cc"></div>
-            {innerWidth < 610 &&
-                <section
-                    className={"d-flex flex-row justify-content-evenly vw-100 h-auto flex-nowrap overflow-scroll mt-4 colour-block"}>
 
-                    {images.slice(0, 3).map(image => (<div className={"col-3 "}>{image}</div>))}
+            <section className="city-firstcarousel colour-block h-fit-content">
+                {images.map(image => (image))}
 
 
-                </section>
-
-                ||
-                <section className="city-firstcarousel colour-block h-fit-content">
-                    {images.map(image => (image))}
-
-
-                </section>
-            }
+            </section>
             {/* <DividerFirstComponent /> */}
             {showDesarrollos &&
-            <section className="propiedades white-block" >
-                {areaDesarrollos.size > 0 &&
-                    <div>
-                        <h3 className="text-center">Propiedades en el Área</h3>
-                    </div>
-                }
-                <br></br>
-                <MDBRow>
-                    {[...areaDesarrollos.values()].map(desarrollo => {
-                        return (<MDBCol xs={12} sm={12} md={6} lg={4} xl={4}>
-                            <Link to={`/desarrollos/${desarrollo.nombre}/`}>
-                                <div
-                                    className="propiedades-img p-0 m-0"
-                                    style={{
-                                        background: `url('https://pagina-mama.s3.amazonaws.com/assets2/areas/${area.name}/${desarrollo.nombre}.webp')`,
-                                        backgroundSize: "cover",
-                                    }}
-                                ></div>
+                <section className="m-0 p-0 propiedades white-block">
+                    {areaDesarrollos.size > 0 &&
+                        <div>
+                            <h3 className="text-center">Propiedades en el Área</h3>
+                        </div>
+                    }
+                    <br></br>
+                    <MDBRow>
+                        {[...areaDesarrollos.values()].map(desarrollo => {
+                            return (<MDBCol xs={12} sm={12} md={6} lg={4} xl={4}>
+                                <Link to={`/desarrollos/${desarrollo.nombre}/`}>
+                                    <div
+                                        className="propiedades-img p-0 m-0"
+                                        style={{
+                                            background: `url('https://pagina-mama.s3.amazonaws.com/assets2/areas/${area.name}/${desarrollo.nombre}.webp')`,
+                                            backgroundSize: "cover",
+                                        }}
+                                    ></div>
 
-                                <h4 className="text-center card-title m-2 ">{desarrollo.nombre.split("-").map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(" ")}</h4>
-                            </Link>
-                        </MDBCol>)
-                    })}
-                </MDBRow>
-            </section>
-}
+                                    <h4 className="text-center card-title m-2 ">{desarrollo.nombre.split("-").map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(" ")}</h4>
+                                </Link>
+                            </MDBCol>)
+                        })}
+                    </MDBRow>
+                </section>
+            }
             {/* <div className="container-fluid m-0 p-0 overflow-scroll horizontal-scrollable"></div> */}
         </>
     );
---
+++ src/components/desarrollos/Brickell/TheMandarinResidencesMiami.tsx
@@ -0,0 +1,71 @@
+import Desarrollo from "../../../models/desarrollos/Desarrollo";
+import Brickell from "../../../objects/areas/Brickell";
+
+
+
+export default function TheMandarinResidencesMiami() {
+
+    const TheMandarinResidencesObject = new Desarrollo(Brickell());
+
+    TheMandarinResidencesObject.nombre = "the-mandarin-oriental-residences-miami";
+    TheMandarinResidencesObject.titulo = <div><h1 className="text-center display-5">The Residences<br>Mandarin Oriental<br>Miami</br></br></h1></div>;
+
+    TheMandarinResidencesObject.amenidades =
+        new Map();
+    TheMandarinResidencesObject.amenidades.set("Ofertas Recreativas",
+        ["Piscina oceánica",
+            "Piscina de la laguna",
+            "Salón junto a la piscina con servicios",
+            "Piscina para niños",
+            "Bosque de hamacas inmersivo",
+            "Paseo artístico del podio",
+            "Simulador de golf de interior",
+            "Sala de juegos",
+            "Sala de proyección privada",
+            "Club de Niños"]);
+
+    TheMandarinResidencesObject.amenidades.set("Experiencias Gastronómicas", [
+        "Pabellón de Comedor",
+        "Pabellón de Té",
+        "Barra de honestidad",
+        "Comedor Privado",
+        "Bodega"
+    ]);
+
+    TheMandarinResidencesObject.amenidades.set("Salud y Bienestar", [
+        "Mandarin Oriental Signature Spa",
+        "Programación de spa por FaceGym™",
+        "Salas de tratamiento interiores/exteriores",
+        "Jardín de té",
+        "Césped de yoga en el bosque",
+        "Saunas y hammam para hombres y mujeres",
+        "Terraza de la piscina de bienestar",
+        "Terapia de frío y calor",
+        "Taller de fitness",
+        "Pista polideportiva",
+        "Balnearios de peluquería canina"
+    ]);
+    TheMandarinResidencesObject.amenidades.set("Eventos de Ocasión y Negocios",[
+        "Suite de conferencias",
+"Oficinas Privadas",
+"Áreas de trabajo de alta gama",
+"Biblioteca",
+"Cocina de autoservicio"
+
+
+    ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return TheMandarinResidencesObject;
+}
